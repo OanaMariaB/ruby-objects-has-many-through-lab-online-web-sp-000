7f5fec0ae308f7030a808067854af4168bde8bc9
Oana Berentan <oana.berentan@yahoo.com>
2019-06-22 15:06:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,31 +1,27 @@
 class Doctor
-  attr_accessor :name
 
+   attr_accessor :name
   @@all = []
 
-  def initialize(name)
+   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def self.all
+   def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+  end
+
+   def patients
+    appointments.map do |appointment|
+      appointment.patient
+  end
+
+   def self.all
     @@all
   end
 
- def new_appointment(date, patient)
-    Appointment.new(date, self, patient)
- end
-
- def appointments
-   Appointment.all.select do |appointment|
-     appointment.doctor == self
-   end
-end
-
- def patients
-   appointments.map do |appointment|
-     appointment.patient
-   end
- end
-
-end
+   def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
